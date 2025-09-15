86f18911f5c06c824ff59d67b0420124b7f60bc3
marc55s <marc.schoenig@gmail.com>
2025-09-15 20:53:04 +0200

    update: lazy lock and lspkind fix

--- lua/core/lazy/lspkind.lua
+++ lua/core/lazy/lspkind.lua
@@ -1,62 +1,26 @@
 return {
-  "onsails/lspkind.nvim",
-  event = "InsertEnter",  -- Use InsertEnter to ensure it loads for autocompletion
-  config = function()
-    require("lspkind").init({
-      mode = "symbol_text", -- Show both symbols and text annotations
-      preset = "codicons",  -- Use VSCode Codicons
-      symbol_map = {
-        Text = "",
-        Method = "",
-        Function = "",
-        Constructor = "",
-        Field = "ﰠ",
-        Variable = "",
-        Class = "ﴯ",
-        Interface = "",
-        Module = "",
-        Property = "ﰠ",
-        Unit = "塞",
-        Value = "",
-        Enum = "",
-        Keyword = "",
-        Snippet = "",
-        Color = "",
-        File = "",
-        Reference = "",
-        Folder = "",
-        EnumMember = "",
-        Constant = "",
-        Struct = "פּ",
-        Event = "",
-        Operator = "",
-        TypeParameter = "",
-      },
-    })
-
-    -- Ensure integration with nvim-cmp
-    local cmp = require("cmp")
-    cmp.setup({
-      formatting = {
-        fields = {'menu', 'abbr', 'kind'},
-        format = require("lspkind").cmp_format({
-          with_text = true,  -- Optional: show text with the icon
-          menu = {
-              nvim_lsp = 'λ',
-              vsnip = '⋗',
-              buffer = 'Ω',
-              path = '🖫',
-          },
-        }),
-      },
-    })
-  end,
     "onsails/lspkind.nvim",
     event = "InsertEnter", -- Use InsertEnter to ensure it loads for autocompletion
     config = function()
+        -- Ensure integration with nvim-cmp
+        local cmp = require("cmp")
+        cmp.setup({
+            formatting = {
+                fields = { 'menu', 'abbr', 'kind' },
+                format = require("lspkind").cmp_format({
+                    with_text = true, -- Optional: show text with the icon
+                    menu = {
+                        nvim_lsp = 'λ',
+                        vsnip = '⋗',
+                        buffer = 'Ω',
+                        path = '🖫',
+                    },
+                }),
+            },
+        })
         require("lspkind").init({
             mode = "symbol_text", -- Show both symbols and text annotations
-            preset = "codicons", -- Use VSCode Codicons
+            preset = "codicons",  -- Use VSCode Codicons
             symbol_map = {
                 Text = "󰉿",
                 Method = "󰆧",
